b838db6fe297e10655677b743a57ceef3a82bc57
Noah Potash <noah.potash@outbreaklabs.com>
2020-05-22 21:18:49 -0400

    Add BlockExtensions

---
+++ GitrbSharp.Tests/BlockExtensionTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+using FluentAssertions;
+using GtirbSharp;
+using GtirbSharp.Extensions;
+
+namespace GitrbSharp.Tests
+{
+    public class BlockExtensionTests
+    {
+        [Fact]
+        public void CalculatesAddressOfCodeBlock()
+        {
+            var interval = new ByteInterval((Section)null);
+            var block = new CodeBlock(interval);
+            interval.Address = 10;
+            block.Offset = 5;
+            block.Address().Should().Be(15);
+        }
+
+        [Fact]
+        public void CalculatesAddressOfDataBlock()
+        {
+            var interval = new ByteInterval((Section)null);
+            var block = new DataBlock(interval);
+            interval.Address = 10;
+            block.Offset = 5;
+            block.Address().Should().Be(15);
+        }
+    }
+}
---
+++ GtirbSharp/Extensions/BlockExtensions.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GtirbSharp.Extensions
+{
+    /// <summary>
+    /// Extension methods for Blocks
+    /// </summary>
+    public static class BlockExtensions
+    {
+        /// <summary>
+        /// Calculate the address of the block
+        /// </summary>
+        public static ulong Address(this CodeBlock block)
+        {
+            if (block.ByteInterval == null) throw new InvalidOperationException("Address can only be calculated if the block belongs to a ByteInterval.");
+            return (block.ByteInterval.Address ?? throw new InvalidOperationException("ByteInterval does not have an address")) + block.Offset;
+        }
+
+        /// <summary>
+        /// Calculate the address of the block
+        /// </summary>
+        public static ulong Address(this DataBlock block)
+        {
+            if (block.ByteInterval == null) throw new InvalidOperationException("Address can only be calculated if the block belongs to a ByteInterval.");
+            return (block.ByteInterval.Address ?? throw new InvalidOperationException("ByteInterval does not have an address")) + block.Offset;
+        }
+    }
+}
